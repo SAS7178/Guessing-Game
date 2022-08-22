f94970aede2a498bb53ec3a9129377310ab6dce7
Stephen Smith <StephenASmith7178@gmail.com>
2022-08-22 15:14:24 -0400

    Phase 3

--- Program.cs
+++ Program.cs
@@ -25,23 +25,81 @@
 // No longer display the user's guess. They know what they guessed, right?
 // Compare the user's guess with the secret number. Display a success message if the guess is correct, otherwise display a failure message.
 
+// int SecretNumber = 40;
+//  CompareToNumber();
+// void CompareToNumber()
+// {
+//     Console.WriteLine("Guess the secret Number: ");
+//     string answer = Console.ReadLine();
+//     int ParsedAnswer = int.Parse(answer);
+
+//         while (String.IsNullOrWhiteSpace(answer))
+
+//         {
+//             Console.WriteLine("Guess the secret number: ");
+//             answer = Console.ReadLine();
+//             ParsedAnswer = int.Parse(answer);
+//         }
+//     if (ParsedAnswer == SecretNumber)
+//      {
+//         Console.WriteLine("Your guess is correct");
+//     }
+// }
+
+// Phase 3
+// The program should be updated to...
+
+// Give the user four chances to guess the number.
+
+
+// Continue to display the success or failure messages as in phase 2
+
+
+///while loop that will check if the count and / or the person has guuessed correctly
+
+//conditions/(person correct or 4 times tried)
+
+//create var set to 0 and each time loop run add ++ to the var
+
+// bool var isAnswercorrect  set equal to false to see if parsed answer is true if soo change bool var to true
+
 int SecretNumber = 40;
+bool isAnswerCorrect = false;
+int timesGuessed = 0;
  CompareToNumber();
+
 void CompareToNumber()
 {
     Console.WriteLine("Guess the secret Number: ");
     string answer = Console.ReadLine();
     int ParsedAnswer = int.Parse(answer);
+    GuessEqualToSecretNumber( ParsedAnswer);
 
-        while (String.IsNullOrWhiteSpace(answer))
-
+        // while (String.IsNullOrWhiteSpace(answer))
+        // {
+        //     Console.WriteLine("Guess the secret number: ");
+        //     answer = Console.ReadLine();
+        //     ParsedAnswer = int.Parse(answer);
+        // }
+        while (isAnswerCorrect == false && timesGuessed < 3)
         {
             Console.WriteLine("Guess the secret number: ");
             answer = Console.ReadLine();
             ParsedAnswer = int.Parse(answer);
+            GuessEqualToSecretNumber( ParsedAnswer);
         }
+    }  
+
+void GuessEqualToSecretNumber(int ParsedAnswer)
+{
+             timesGuessed += 1;
+
     if (ParsedAnswer == SecretNumber)
      {
         Console.WriteLine("Your guess is correct");
+        isAnswerCorrect = true;
+    } else {
+        Console.WriteLine($"You guessed {timesGuessed} of 4 tries");
     }
 }
+
