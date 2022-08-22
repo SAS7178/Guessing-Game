ece95ed652afe5c0310a984c66e05152072fb78c
Stephen Smith <StephenASmith7178@gmail.com>
2022-08-22 11:44:34 -0400

    phase one

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+// The program should...
+
+// Display a message to the user asking them to guess the secret number.
+// Display a prompt for the user's guess.
+// Take the user's guess as input and save it as a variable.
+// Display the user's guess back to the screen.
+
+//prompt needs to prrint to screen(WriteLine)
+    Console.WriteLine("Guess the secret number");
+
+//user needs to respond(Input/REadLine--reades input)
+//save user response aas variable()
+    string answer = Console.ReadLine();
+    int ParsedAnswer = int.Parse(answer);
+
+//show users guess back to them (writeLine--your guess is..)
+Console.WriteLine($"Your guess is {ParsedAnswer}");
+
